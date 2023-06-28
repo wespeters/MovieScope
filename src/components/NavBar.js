a8fa6b7202107522791f7938473bce62a18e2276
John Phonxaya <phonxayajohn@gmail.com>
2023-06-28 13:31:39 -0500

    CSS theme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ const NavBar = () => {
     return (
         <nav className="navbar">
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/add-movie">Add Movie</Link></li>
+                <li><Link to="/" className='navlink'>Home</Link></li>
+                <li><Link to="/add-movie" className='navlink'>Add Movie</Link></li>
             </ul>
         </nav>
     );
